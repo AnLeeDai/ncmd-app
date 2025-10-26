e9a8bb837a605cfc6691268f5be9890d94fa87db
An Le Dai <ledaian22@gmail.com>
2025-10-26 23:41:47 +0700

    updated

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,6 +6,8 @@ import * as React from "react";
 import { HeroUIProvider } from "@heroui/system";
 import { useRouter } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export interface ProvidersProps {
   children: React.ReactNode;
@@ -25,7 +27,10 @@ export function Providers({ children, themeProps }: ProvidersProps) {
 
   return (
     <HeroUIProvider navigate={router.push}>
-      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+      <NextThemesProvider {...themeProps}>{children}
+        <Analytics />
+        <SpeedInsights />
+      </NextThemesProvider>
     </HeroUIProvider>
   );
 }
